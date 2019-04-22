76f8e9f736cc21e73f2ee557f2c88b3998738724
HDC-326 <huangdc521@gmail.com>
2019-04-22 20:38:38 +0800

    Modified unit test

--- __test__/test.spec.js
+++ __test__/test.spec.js
@@ -4,7 +4,7 @@ import case3 from '../src/case3';
 
 describe('test', function () {
   it('case1', function () {
-    expect(case1).toBe([3, 1, 2]);
+    expect(case1).toEqual([3, 1, 2]);
   });
   
   it('case2', async function () {
@@ -15,11 +15,11 @@ describe('test', function () {
       Promise.resolve(4)
     ];
     const result = await case2(tasks);
-    expect(result).toBe([1, 2, null, 4]);
+    expect(result).toEqual([1, 2, null, 4]);
   });
   
   it('case3', function () {
     const arr = [1, [2, [3, [4]]]];
-    expect(case3(arr, 2)).toBe([1, 2, 3, [4]])
+    expect(case3(arr, 2)).toEqual([1, 2, 3, [4]])
   });
 });
