4ebf947e4044768f8164ddc460b6d3a934fdbe97
Roy.idoodle <idoodle@aliyun.com>
2019-10-21 11:13:22 +0800

    Update case2.js

--- src/case2.js
+++ src/case2.js
@@ -2,6 +2,21 @@
  * 异步任务队列
  * */
 
-export default function (tasks /* 每一项的类型为 () => Promise<any> */) {
-
+export default function excute(tasks) {
+	let resultList = []
+	return tasks.reduce(
+    (previousPromise, currentPromise) => previousPromise.then(() => {
+		return new Promise(resolve => {
+			let promise = typeof currentPromise === 'function'? currentPromise(): currentPromise;
+			promise.then(result => {
+				resultList.push(result)
+				resolve()
+			}).catch(() => {
+				resultList.push(null)
+				resolve()
+			})
+		})
+	}),
+    Promise.resolve()
+	).then(last => resultList)
 }
